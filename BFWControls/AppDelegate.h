9e716746bd3104ce853eae787909c8e67163dee9
Tom Brodhurst-Hill <developer@barefeetware.com>
2015-11-05 11:17:08 +1100

    Initial Commit

---
+++ BFWControls/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BFWControls
+//
+//  Created by Tom Brodhurst-Hill on 5/11/2015.
+//  Copyright © 2015 BareFeetWare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BFWControls/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BFWControls
+//
+//  Created by Tom Brodhurst-Hill on 5/11/2015.
+//  Copyright © 2015 BareFeetWare. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
