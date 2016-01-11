37f6b2add10f4457eaf1a0bd85d1b7deaf4d5328
Tom Brodhurst-Hill <developer@barefeetware.com>
2016-01-12 10:58:19 +1100

    BlurView: converted code to Swift

--- BFWControls/Launch/BFWControls-Bridging-Header.h
+++ BFWControls/Launch/BFWControls-Bridging-Header.h
@@ -2,3 +2,6 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+// For BlurView.swift:
+#import "UIImageEffects.h"
+#import "UIImage+BFW.h"
--- BFWControls/Modules/Blur/View/BlurView.swift
+++ BFWControls/Modules/Blur/View/BlurView.swift
@@ -1,76 +1,51 @@
 //
-//  BFWBlurView.m
+//  BlurView.swift
 //
 //  Created by Tom Brodhurst-Hill on 13/10/2015.
+//  Copyright (c) 2015 BareFeetWare. All rights reserved.
+//  Free to use at your own risk, with acknowledgement to BareFeetWare.
 //
 
-#import "BFWBlurView.h"
-#import "UIImageEffects.h"
-#import "UIImage+BFW.h"
+import UIKit
 
-@interface BFWBlurView ()
+class BlurView: UIView {
 
-@property (nonatomic, assign) IBInspectable CGFloat blurRadius;
-
-@end
-
-@implementation BFWBlurView
-
-#pragma mark - init
-
-- (instancetype)initWithFrame:(CGRect)frame {
-    self = [super initWithFrame:frame];
-    if (self) {
-        [self commonInit];
+    @IBInspectable var blurRadius: CGFloat = 10.0 {
+        didSet {
+            setNeedsDisplay()
+        }
     }
-    return self;
-}
 
-- (instancetype)initWithCoder:(NSCoder *)aDecoder {
-    self = [super initWithCoder:aDecoder];
-    if (self) {
-        [self commonInit];
+    //MARK: Accessors
+
+    var blurredImage: UIImage? {
+        var blurredImage: UIImage? = nil;
+        if let subviews = superview?.subviews,
+            selfIndex = subviews.indexOf(self) {
+            if selfIndex > 0 {
+                let previousView = subviews[selfIndex - 1];
+                let contentImage = UIImage(ofView: previousView,
+                                           size: bounds.size)
+                blurredImage = UIImageEffects.imageByApplyingBlurToImage(contentImage,
+                                                                         withRadius: blurRadius,
+                                                                         tintColor: backgroundColor,
+                                                                         saturationDeltaFactor: 1.8,
+                                                                         maskImage: nil)
+            }
+        }
+        return blurredImage
     }
-    return self;
-}
 
-- (void)commonInit {
-    _blurRadius = 10.0;
-}
+    //MARK: UIView
 
-#pragma mark - accessors
-
-- (void)setBlurRadius:(CGFloat)blurRadius {
-    _blurRadius = blurRadius;
-    [self setNeedsDisplay];
-}
-
-- (UIImage *)blurredImage {
-    UIImage *blurredImage = nil;
-    NSArray *subviews = self.superview.subviews;
-    NSUInteger selfIndex = [subviews indexOfObject:self];
-    if (selfIndex > 0) {
-        UIView *previousView = subviews[selfIndex - 1];
-        UIImage *contentImage = [UIImage imageOfView:previousView
-                                                size:self.bounds.size];
-        blurredImage = [UIImageEffects imageByApplyingBlurToImage:contentImage
-                                                       withRadius:self.blurRadius
-                                                        tintColor:self.backgroundColor
-                                            saturationDeltaFactor:1.8
-                                                        maskImage:nil];
+    #if TARGET_INTERFACE_BUILDER
+    // Dont draw anything, so it is transparent.
+    #else
+    override func drawRect(_: CGRect) {
+        if let image = blurredImage {
+            image.drawInRect(bounds)
+        }
     }
-    return blurredImage;
+    #endif
 }
 
-#pragma mark - UIView
-
-#if TARGET_INTERFACE_BUILDER
-// Dont draw anything, so it is transparent.
-#else
-- (void)drawRect:(CGRect)rect {
-    UIImage *image = [self blurredImage];
-    [image drawInRect:self.bounds];
-}
-#endif
-
-@end
