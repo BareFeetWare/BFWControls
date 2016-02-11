5fed924ea563475d29431240d8055fdba2860f73
Tom Brodhurst-Hill <developer@barefeetware.com>
2016-02-12 07:48:26 +1100

    MorphSegue: added commented out initial code to move subviews via constraints instead of frames. Still executes old frame functionality.

---
+++ BFWControls/Modules/Layout/View/NSLayoutConstraintExtensions.swift
@@ -0,0 +1,100 @@
+//
+//  NSLayoutConstraintExtensions.swift
+//  BFWControls
+//
+//  Created by Tom Brodhurst-Hill on 11/02/2016.
+//  Copyright © 2016 BareFeetWare. All rights reserved.
+//
+
+import UIKit
+
+extension NSLayoutConstraint {
+    
+    func constraintWithMultiplier(multiplier: CGFloat) -> NSLayoutConstraint {
+        let constraint = NSLayoutConstraint(
+            item: firstItem,
+            attribute: firstAttribute,
+            relatedBy: relation,
+            toItem: secondItem,
+            attribute: secondAttribute,
+            multiplier:multiplier,
+            constant: constant
+        )
+        return constraint;
+    }
+
+    class func constraintsReplacingConstraints(
+        oldConstraints: [NSLayoutConstraint],
+        views oldViews: [UIView],
+        withViews views: [UIView]
+        ) -> [NSLayoutConstraint]
+    {
+        var constraints = [NSLayoutConstraint]()
+        for oldConstraint in oldConstraints {
+            if var firstItem = oldConstraint.firstItem as? UIView,
+                var secondItem = oldConstraint.secondItem as? UIView
+            {
+                var didReplace = false
+                if oldViews.contains(firstItem) {
+                    firstItem = views[oldViews.indexOf(firstItem)!];
+                    didReplace = true
+                }
+                if oldViews.contains(secondItem) {
+                    secondItem = views[oldViews.indexOf(secondItem)!]
+                    didReplace = true
+                }
+                if didReplace {
+                    let constraint = NSLayoutConstraint(
+                        item: firstItem,
+                        attribute: oldConstraint.firstAttribute,
+                        relatedBy: oldConstraint.relation,
+                        toItem: secondItem,
+                        attribute: oldConstraint.secondAttribute,
+                        multiplier: oldConstraint.multiplier,
+                        constant: oldConstraint.constant
+                    )
+                    constraints.append(constraint)
+                }
+            }
+        }
+        return constraints
+    }
+
+    class func copyDescendantConstraintsFromSourceView(sourceView: UIView, toDestinationView destinationView: UIView) {
+        var sourceConstraints = [NSLayoutConstraint]()
+        var destinationConstraints = [NSLayoutConstraint]()
+        var sourceSubviews = [UIView]()
+        var destinationSubviews = [UIView]()
+        sourceConstraints.appendContentsOf(sourceView.constraints)
+        destinationConstraints.appendContentsOf(destinationView.constraints)
+        sourceSubviews.append(sourceView) // TODO: add top/bottom guides
+        destinationSubviews.append(destinationView) // TODO: add top/bottom guides
+//        for sourceSubview in contentView!.subviews {
+        for sourceSubview in sourceView.subviews {
+            if let destinationSubview = destinationView.subviewMatchingView(sourceSubview) {
+                if sourceSubview.tag == 1 { // testing using tag
+                    sourceSubviews.append(sourceSubview)
+                    destinationSubviews.append(destinationSubview)
+                    for constraint in sourceView.constraints {
+                        if constraint.firstItem as? UIView == sourceSubview || constraint.secondItem as? UIView == sourceSubview {
+                            sourceConstraints.append(constraint)
+                        }
+                    }
+                    for constraint in destinationView.constraints {
+                        if constraint.firstItem as? UIView == destinationSubview || constraint.secondItem as? UIView == destinationSubview {
+                            destinationConstraints.append(constraint)
+                        }
+                    }
+                }
+            }
+        }
+        let constraints = constraintsReplacingConstraints(
+            destinationConstraints,
+            views: destinationSubviews,
+            withViews: sourceSubviews
+        )
+        NSLayoutConstraint.deactivateConstraints(sourceConstraints)
+        NSLayoutConstraint.activateConstraints(constraints)
+    }
+    
+}
--- BFWControls/Modules/Segues/Controller/MorphSegue.swift
+++ BFWControls/Modules/Segues/Controller/MorphSegue.swift
@@ -17,47 +17,50 @@ class MorphSegue: UIStoryboardSegue {
         return self.sourceViewController.view
     }()
     
-    var duration: NSTimeInterval  = 1.0
+    var duration: NSTimeInterval = 1.0
     var animationOptions = UIViewAnimationOptions.CurveEaseInOut
-
-    // MARK: - UIStoryboardSegue
-
-    override func perform() {
-        let destinationViewController = self.destinationViewController;
-        let destinationVCView = destinationViewController.view;
-        let sourceVCView = self.sourceViewController.view;
     
+    // MARK: - UIStoryboardSegue
+    
+    override func perform() {
+        let destinationVCView = destinationViewController.view
+        let sourceVCView = sourceViewController.view
+        
         // Force frames to match constraints, in case misplaced.
         destinationVCView.setNeedsLayout()
         destinationVCView.layoutIfNeeded()
-    
+        
         destinationVCView.frame = sourceVCView.frame
         if let cell = fromView as? UITableViewCell {
             cell.selected = false
         }
+        
         var morphingView: UIView?
         var contentView: UIView?
         let isMorphingCopy = true
         if isMorphingCopy {
             // Create a copy of the view hierarchy for morphing, so the original is not changed.
             if let cell = fromView as? UITableViewCell {
-                morphingView = cell.copyWithSubviews( nil,
-                    includeConstraints: false)
-                morphingView?.copySubviews(cell.contentView.subviews,
-                    includeConstraints:false)
+                morphingView = cell.copyWithSubviews(nil, includeConstraints: false)
+                morphingView?.copySubviews(cell.contentView.subviews, includeConstraints: false)
             } else {
-                morphingView = fromView?.copyWithSubviews(fromView?.subviews,
-                    includeConstraints: false)
+                morphingView = fromView?.copyWithSubviews(fromView?.subviews, includeConstraints: false)
             }
-            sourceVCView.addSubview(morphingView!)
-            contentView = morphingView!
+            if let morphingView = morphingView {
+                sourceVCView.addSubview(morphingView)
+                contentView = morphingView
+            }
         } else {
-            // morphingView = self.fromView;
+            // morphingView = fromView
             // if let cell = fromView as? UITableViewCell {
-            //     contentView = cell.contentView;
+            //     contentView = cell.contentView
             // }
             // TODO: finish
         }
+        
+        // Add destination constraints, which will animate frames when layout is updated, inside animation block below.
+//        NSLayoutConstraint.copyDescendantConstraintsFromSourceView(sourceVCView, toDestinationView: destinationVCView)
+        
         if let morphingView = morphingView {
             UIView.animateWithDuration(
                 duration,
@@ -65,8 +68,10 @@ class MorphSegue: UIStoryboardSegue {
                 options: animationOptions,
                 animations: {
                     morphingView.frame = sourceVCView.bounds;
+//                    morphingView.setNeedsLayout()
+//                    morphingView.layoutIfNeeded()
                     if let _ = self.fromView as? UITableViewCell {
-                        contentView?.frame = morphingView.bounds;
+                        contentView?.frame = morphingView.bounds
                     }
                     if let subviews = contentView?.subviews {
                         for subview in subviews {
@@ -80,7 +85,7 @@ class MorphSegue: UIStoryboardSegue {
                     }
                 })
                 { finished in
-                    self.sourceViewController.navigationController?.pushViewController(destinationViewController,
+                    self.sourceViewController.navigationController?.pushViewController(self.destinationViewController,
                         animated: false)
                     if isMorphingCopy {
                         morphingView.removeFromSuperview()
