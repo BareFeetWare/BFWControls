a1902a1b0b927308e7996fa2062c26d1cb570d4f
Tom Brodhurst-Hill <developer@barefeetware.com>
2016-03-21 18:47:55 +1100

    SideMenuSegue: added

---
+++ BFWControls/Modules/Segues/View/SideMenuSegue.swift
@@ -0,0 +1,99 @@
+//
+//  SideMenuSegue.swift
+//
+//  Created by Tom Brodhurst-Hill on 20/03/2016.
+//  Copyright © 2016 BareFeetWare. All rights reserved.
+//
+
+import UIKit
+
+class SideMenuSegue: UIStoryboardSegue, UIViewControllerTransitioningDelegate {
+
+    override func perform() {
+        destinationViewController.transitioningDelegate = self
+        destinationViewController.modalPresentationStyle = .Custom
+        sourceViewController.navigationController?.presentViewController(self.destinationViewController, animated: true, completion: nil)
+    }
+    
+    // MARK: - UIViewControllerTransitioningDelegate
+    
+    func animationControllerForPresentedController(
+        presented: UIViewController,
+        presentingController presenting: UIViewController,
+        sourceController source: UIViewController
+        ) -> UIViewControllerAnimatedTransitioning?
+    {
+        return SideMenuTransition()
+    }
+    
+    func animationControllerForDismissedController(dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return SideMenuDismissTransition()
+    }
+    
+}
+
+class SideMenuTransition: NSObject, UIViewControllerAnimatedTransitioning {
+    
+    func transitionDuration(transitionContext: UIViewControllerContextTransitioning?) -> NSTimeInterval {
+        return 3.0
+    }
+    
+    @objc func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
+        
+        if let containerView = transitionContext.containerView(),
+            let presentedViewController = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey),
+            let presentedView = presentedViewController.view
+        {
+            containerView.addSubview(presentedView)
+            let peekWidth: CGFloat = 44.0
+            let sideMenuWidth = containerView.frame.size.width - peekWidth
+            presentedView.frame = CGRect(
+                x: -sideMenuWidth,
+                y: 0.0,
+                width: sideMenuWidth,
+                height: containerView.frame.size.height
+            )
+            UIView.animateWithDuration(
+                0.5,
+                delay: 0.0,
+                options: UIViewAnimationOptions.CurveEaseInOut,
+                animations: {
+                    presentedView.frame.origin.x = 0.0
+                }
+                )
+                { finished in
+                    transitionContext.completeTransition(true)
+            }
+        }
+    }
+    
+}
+
+class SideMenuDismissTransition: NSObject, UIViewControllerAnimatedTransitioning {
+    
+    func transitionDuration(transitionContext: UIViewControllerContextTransitioning?) -> NSTimeInterval {
+        return 3.0
+    }
+    
+    @objc func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
+        
+        if // let containerView = transitionContext.containerView(),
+            let presentedViewController = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey),
+            let presentedView = presentedViewController.view
+        {
+            UIView.animateWithDuration(
+                0.5,
+                delay: 0.0,
+                options: UIViewAnimationOptions.CurveEaseInOut,
+                animations: {
+                    presentedView.frame.origin.x = -presentedView.frame.size.width
+                }
+                )
+                { finished in
+                    transitionContext.completeTransition(true)
+                    presentedView.removeFromSuperview()
+            }
+        }
+    }
+    
+}
