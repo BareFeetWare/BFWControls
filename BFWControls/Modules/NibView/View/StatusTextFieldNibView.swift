9b1dd5413184e72c2901310cbd4952f4e42208de
Tom Brodhurst-Hill <developer@barefeetware.com>
2016-05-24 13:25:19 +1000

    StatusTextFieldNibView: IBOutlets: iconView, titleLabel, messageLabel, borderView

---
+++ BFWControls/Modules/NibView/View/StatusTextFieldNibView.swift
@@ -0,0 +1,20 @@
+//
+//  StatusTextFieldNibView.swift
+//  BFWControls
+//
+//  Created by Tom Brodhurst-Hill on 24/05/2016.
+//  Copyright © 2016 BareFeetWare. Free to use and modify, without warranty.
+//
+
+import UIKit
+
+class StatusTextFieldNibView: NibView {
+
+    // MARK: - IBOutlets
+
+    @IBOutlet weak var iconView: UIView?
+    @IBOutlet weak var titleLabel: UILabel?
+    @IBOutlet weak var messageLabel: UILabel?
+    @IBOutlet weak var borderView: UIView?
+    
+}
