e2dd051342443be88955a2daa4799e4e0e1e2ce6
Tom Brodhurst-Hill <developer@barefeetware.com>
2016-05-24 10:03:20 +1000

    ActionSheetView: subclass of AlertView, with Action Sheet layout

---
+++ BFWControls/Modules/Alert/View/ActionSheetView.swift
@@ -0,0 +1,14 @@
+//
+//  ActionSheetView.swift
+//  BFWControls
+//
+//  Created by Tom Brodhurst-Hill on 23/02/2016.
+//  Copyright © 2016 BareFeetWare.
+//  Free to use at your own risk, with acknowledgement to BareFeetWare.
+//
+
+import UIKit
+
+class ActionSheetView: AlertView {
+
+}
