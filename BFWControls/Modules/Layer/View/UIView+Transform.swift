53bc1d07a3fb70a9b671b4ac6da769225e310c5f
Tom Brodhurst-Hill <developer@barefeetware.com>
2016-08-09 12:00:17 +1000

    UIView+Layer: Renamed from LayerView UIView+Transform: rotationDegrees

---
+++ BFWControls/Modules/Layer/View/UIView+Transform.swift
@@ -0,0 +1,24 @@
+//
+//  LayerView.swift
+//  BFWControls
+//
+//  Created by Tom Brodhurst-Hill on 9/01/2016.
+//  Copyright © 2016 BareFeetWare.
+//  Free to use at your own risk, with acknowledgement to BareFeetWare.
+//
+
+import UIKit
+
+extension UIView {
+    
+    var rotationDegrees: CGFloat {
+        get {
+            // TODO: Reverse engineer angle from transform.
+            return 0.0
+        }
+        set {
+            transform = CGAffineTransformMakeRotation(CGFloat(M_PI) / 180.0 * newValue)
+        }
+    }
+    
+}
