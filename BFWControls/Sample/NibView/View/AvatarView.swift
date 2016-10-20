d6a8545af58106f82ded6f6045895c32d0535960
Tom Brodhurst-Hill <developer@barefeetware.com>
2016-10-20 15:40:59 +1100

    Samples/NibView added

---
+++ BFWControls/Sample/NibView/View/AvatarView.swift
@@ -0,0 +1,14 @@
+//
+//  AvatarView.swift
+//  BFWControls
+//
+//  Created by Tom Brodhurst-Hill on 17/10/16.
+//  Copyright © 2016 BareFeetWare. All rights reserved.
+//  Free to use at your own risk, with acknowledgement to BareFeetWare.
+//
+
+import UIKit
+
+@IBDesignable class AvatarView: NibCellView {
+    
+}
