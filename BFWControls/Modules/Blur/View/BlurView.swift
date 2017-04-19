181f7124817e08059a00563d361b2c9b4e9c4b06
Charlotte Tortorella <charlotte@monadic.consulting>
2017-04-19 11:25:44 +1000

    Change blurredImage to use safer code

--- BFWControls/Modules/Blur/View/BlurView.swift
+++ BFWControls/Modules/Blur/View/BlurView.swift
@@ -19,21 +19,18 @@ class BlurView: UIView {
     //MARK: Accessors
 
     var blurredImage: UIImage? {
-        var blurredImage: UIImage? = nil;
-        if let subviews = superview?.subviews,
-            let selfIndex = subviews.index(of: self) {
-            if selfIndex > 0 {
-                let previousView = subviews[selfIndex - 1];
-                let contentImage = UIImage(of: previousView,
-                                           size: bounds.size)
-                blurredImage = UIImageEffects.imageByApplyingBlur(to: contentImage,
-                                                                         withRadius: blurRadius,
-                                                                         tintColor: backgroundColor,
-                                                                         saturationDeltaFactor: 1.8,
-                                                                         maskImage: nil)
-            }
-        }
-        return blurredImage
+        guard let subviews = superview?.subviews,
+            let previousIndex = subviews.index(of: self).map({ $0 - 1 }),
+            subviews.indices.contains(previousIndex)
+            else { return nil }
+        let contentImage = UIImage(of: subviews[previousIndex],
+                                   size: bounds.size)
+        return UIImageEffects
+            .imageByApplyingBlur(to: contentImage,
+                                 withRadius: blurRadius,
+                                 tintColor: backgroundColor,
+                                 saturationDeltaFactor: 1.8,
+                                 maskImage: nil)
     }
 
     //MARK: UIView
