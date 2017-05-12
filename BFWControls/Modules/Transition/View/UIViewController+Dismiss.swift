0cda501fc52a8b2617fe6a38ed14b7b4cdc51e53
Tom Brodhurst-Hill <developer@barefeetware.com>
2017-05-12 13:39:46 +1000

    UIViewController: dismiss(_ sender: UIControl?)

---
+++ BFWControls/Modules/Transition/View/UIViewController+Dismiss.swift
@@ -0,0 +1,17 @@
+//
+//  UIViewController+Dismiss.swift
+//  BFWControls
+//
+//  Created by Tom Brodhurst-Hill on 12/5/17.
+//  Copyright © 2017 BareFeetWare. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    @IBAction func dismiss(_ sender: UIControl?) {
+        dismiss(animated: true) {}
+    }
+    
+}
