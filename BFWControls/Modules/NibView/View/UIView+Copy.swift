4e0cea31752e33b9da1ca5299c726320bfc88ef3
Tom Brodhurst-Hill <developer@barefeetware.com>
2017-06-30 11:54:26 +1000

    UIView+Copy: Converted code to Swift.

--- BFWControls/Modules/NibView/View/UIView+Copy.swift
+++ BFWControls/Modules/NibView/View/UIView+Copy.swift
@@ -5,209 +5,193 @@
 //  Copyright © 2016 BareFeetWare. Free to use and modify, without warranty.
 //
 
-#import "UIKit.h"
+import UIKit
 
-@interface UIView (BFWCopy)
-
-#pragma mark - class methods
-
-+ (NSBundle *)bundle;
-+ (CGSize)sizeFromNib;
-
-#pragma mark - instance methods
-
-- (UIView *)viewFromNib;
-- (UIView *)copyWithSubviews:(NSArray *)subviews
-          includeConstraints:(BOOL)includeConstraints;
-- (void)copySubviews:(NSArray *)subviews
-  includeConstraints:(BOOL)includeConstraints;
-- (void)copyConstraintsFromView:(UIView *)view;
-- (void)copyPropertiesFromView:(UIView *)view;
-- (void)copyAnimatablePropertiesFromView:(UIView *)fromView;
-
-@end
-
-@implementation UIView (BFWCopy)
-
-#pragma mark - class methods
-
-+ (NSBundle *)bundle {
-    BOOL isInterfaceBuilder;
-#if TARGET_INTERFACE_BUILDER // Rendering in storyboard using IBDesignable.
-    isInterfaceBuilder = YES;
-#else
-    isInterfaceBuilder = NO;
-#endif
-    NSBundle *bundle = isInterfaceBuilder
-    ? [NSBundle bundleForClass:self]
-    // TODO: Dynamic strings:
-    : [self.moduleName isEqualToString:@"BFWControls"]
-    ? [NSBundle bundleWithIdentifier:@"com.barefeetware.BFWControls"]
-    : [NSBundle mainBundle];
-    return bundle;
-}
-
-+ (NSArray *)classNameComponents {
-    NSString *fullClassName = NSStringFromClass(self);
-    return [fullClassName componentsSeparatedByString:@"."];
-}
-
-+ (NSString *)nibName {
-    // Remove the <ProjectName>. prefix that Swift adds:
-    return self.classNameComponents.lastObject;
-}
-
-+ (NSString *)moduleName {
-    NSArray* components = self.classNameComponents;
-    return components.count > 1
-    ? components.firstObject
-    : nil;
-}
-
-+ (CGSize)sizeFromNib {
-    NSArray *nibViews = [[self bundle] loadNibNamed:[self nibName] owner:nil options:nil];
-    UIView *nibView = nibViews.firstObject;
-    CGSize size = nibView.frame.size;
-    return size;
-}
-
-#pragma mark - instance methods
-
-- (void)copyConstraintsFromView:(UIView *)view {
-    self.translatesAutoresizingMaskIntoConstraints = view.translatesAutoresizingMaskIntoConstraints;
-    for (NSLayoutConstraint *constraint in view.constraints) {
-        id firstItem = constraint.firstItem;
-        id secondItem = constraint.secondItem;
-        if (firstItem) {
-            if (firstItem == view) {
-                firstItem = self;
+public extension UIView {
+    
+    // MARK: - Class variables
+    
+    public static var bundle: Bundle? {
+        let isInterfaceBuilder: Bool
+        #if TARGET_INTERFACE_BUILDER // Rendering in storyboard using IBDesignable.
+            isInterfaceBuilder = true
+        #else
+            isInterfaceBuilder = false
+        #endif
+        let bundle = isInterfaceBuilder
+            ? Bundle(for: self)
+                // TODO: Dynamic strings:
+            : moduleName == "BFWControls"
+            ? Bundle(identifier: "com.barefeetware.BFWControls")
+            : Bundle.main
+        return bundle
+    }
+    
+    static var classNameComponents: [String] {
+        let fullClassName = String(describing: self)
+        return fullClassName.components(separatedBy: ".")
+    }
+    
+    static var nibName: String {
+        // Remove the <ProjectName>. prefix that Swift adds:
+        return classNameComponents.last!
+    }
+    
+    static var moduleName: String? {
+        let components = classNameComponents
+        return components.count > 1
+            ? components.first!
+            : nil
+    }
+    
+    static var sizeFromNib: CGSize? {
+        return (bundle?
+            .loadNibNamed(nibName, owner:nil, options: [:])?
+            .first as? UIView)?
+            .frame.size
+    }
+    
+    // MARK: - Instance functions
+    
+    func copyConstraints(from view: UIView) {
+        translatesAutoresizingMaskIntoConstraints = view.translatesAutoresizingMaskIntoConstraints
+        for constraint in view.constraints {
+            if var firstItem = constraint.firstItem as? UIView,
+                var secondItem = constraint.secondItem as? UIView
+            {
+                if firstItem == view {
+                    firstItem = self
+                }
+                if secondItem == view {
+                    secondItem = self
+                }
+                let copiedConstraint = NSLayoutConstraint(
+                    item: firstItem,
+                    attribute: constraint.firstAttribute,
+                    relatedBy: constraint.relation,
+                    toItem: secondItem,
+                    attribute: constraint.secondAttribute,
+                    multiplier: constraint.multiplier,
+                    constant: constraint.constant
+                )
+                addConstraint(copiedConstraint)
+            } else {
+                debugPrint("copyConstraintsFromView: error: firstItem is not a UIView")
             }
-            if (secondItem == view) {
-                secondItem = self;
+            for axis in [UILayoutConstraintAxis.horizontal, UILayoutConstraintAxis.vertical] {
+                setContentCompressionResistancePriority(view.contentCompressionResistancePriority(for: axis), for: axis)
+                setContentHuggingPriority(view.contentHuggingPriority(for: axis), for: axis)
             }
-            NSLayoutConstraint *copiedConstraint;
-            copiedConstraint = [NSLayoutConstraint constraintWithItem:firstItem
-                                                            attribute:constraint.firstAttribute
-                                                            relatedBy:constraint.relation
-                                                               toItem:secondItem
-                                                            attribute:constraint.secondAttribute
-                                                           multiplier:constraint.multiplier
-                                                             constant:constraint.constant];
-            [self addConstraint:copiedConstraint];
-        }
-        else {
-            NSLog(@"copyConstraintsFromView: error: firstItem == nil");
         }
     }
-    for (UILayoutConstraintAxis axis = UILayoutConstraintAxisHorizontal; axis <= UILayoutConstraintAxisVertical; axis += UILayoutConstraintAxisVertical - UILayoutConstraintAxisHorizontal) {
-        [self setContentCompressionResistancePriority:[view contentCompressionResistancePriorityForAxis:axis] forAxis:axis];
-        [self setContentHuggingPriority:[view contentHuggingPriorityForAxis:axis] forAxis:axis];
+    
+    var viewFromNib: UIView? {
+        let nibName = type(of: self).nibName
+        guard let nibViews = type(of: self).bundle?.loadNibNamed(nibName, owner: nil, options: nil),
+            let nibView = nibViews.first(where: { type(of: $0) == type(of: self) } ) as? UIView
+            else {
+                debugPrint("**** error: Could not find an instance of class \(type(of: self)) in \(nibName) xib")
+                return nil
+        }
+        (nibView as? Morphable)?.copyProperties(from: self)
+        nibView.copyConstraints(from: self)
+        return nibView
     }
-}
-
-- (UIView *)viewFromNib {
-    NSString *nibName = [[self class] nibName];
-    NSArray *nibViews = [[[self class] bundle] loadNibNamed:nibName owner:nil options:nil];
-    UIView *nibView = nil;
-    for (UIView *view in nibViews) {
-        if ([view isKindOfClass:[self class]]) {
-            nibView = view;
-            break;
+    
+    var copied: UIView {
+        let copiedView = type(of: self).init(frame: frame)
+        (copiedView as? Morphable)?.copyProperties(from: self)
+        return copiedView
+    }
+    
+    func copied(withSubviews subviews: [UIView],
+                includeConstraints: Bool) -> UIView {
+        let copiedView = copied
+        copiedView.copySubviews(subviews,
+                                includeConstraints: includeConstraints)
+        return copiedView
+    }
+    
+    func copySubviews(_ subviews: [UIView],
+                      includeConstraints: Bool)
+    {
+        for subview in subviews {
+            let copiedSubview = type(of: subview).init(frame: frame)
+            addSubview(copiedSubview)
+            (copiedSubview as? Morphable)?.copyProperties(from: subview)
+            if includeConstraints {
+                copiedSubview.copyConstraints(from: subview)
+            }
+            copiedSubview.copySubviews(subview.subviews,
+                                       includeConstraints: includeConstraints)
         }
     }
-    if (!nibView) {
-        NSLog(@"**** error: Could not find an instance of class \"%@\" in \"%@\" xib", NSStringFromClass([self class]), nibName);
-    } else {
-        [nibView copyPropertiesFromView:self];
-        [nibView copyConstraintsFromView:self];
-    }
-    return nibView;
-}
-
-- (instancetype)copy {
-    UIView *copiedView = [[[self class] alloc] initWithFrame:self.frame];
-    [copiedView copyPropertiesFromView:self];
-    return copiedView;
-}
-
-- (UIView *)copyWithSubviews:(NSArray *)subviews
-          includeConstraints:(BOOL)includeConstraints {
-    UIView *copiedView = [self copy];
-    [copiedView copySubviews:subviews
-          includeConstraints:includeConstraints];
-    return copiedView;
-}
-
-- (void)copySubviews:(NSArray *)subviews
-  includeConstraints:(BOOL)includeConstraints
-{
-    for (UIView* subview in subviews) {
-        UIView *copiedSubview = [[[subview class] alloc] initWithFrame:subview.frame];
-        [self addSubview:copiedSubview];
-        [copiedSubview copyPropertiesFromView:subview];
-        if (includeConstraints) {
-            [copiedSubview copyConstraintsFromView:subview];
+    
+    func copyAnimatableProperties(from view: UIView) {
+        alpha = view.alpha
+        if view.backgroundColor != nil {
+            backgroundColor = view.backgroundColor
         }
-        [copiedSubview copySubviews:subview.subviews
-                 includeConstraints:includeConstraints];
+        transform = view.transform
     }
+    
 }
 
-- (void)copyPropertiesFromView:(UIView *)view {
-    [self copyAnimatablePropertiesFromView:view];
-    self.frame = view.frame;
-    self.tag = view.tag;
-    self.userInteractionEnabled = view.userInteractionEnabled;
-    self.hidden = view.hidden;
+public protocol Morphable {
+    
+    func copyProperties(from view: UIView)
+    
 }
 
-- (void)copyAnimatablePropertiesFromView:(UIView *)view {
-    self.alpha = view.alpha;
-    if (view.backgroundColor) {
-        self.backgroundColor = view.backgroundColor;
+public extension Morphable where Self: UIView {
+    
+    func copyProperties(from view: UIView) {
+        copyAnimatableProperties(from: view)
+        frame = view.frame
+        tag = view.tag
+        isUserInteractionEnabled = view.isUserInteractionEnabled
+        isHidden = view.isHidden
     }
-    self.transform = view.transform;
+    
 }
 
-@end
-
-@implementation UILabel (BFWMorph)
-
-- (void)copyPropertiesFromView:(UILabel*)view {
-    [super copyPropertiesFromView:view];
-    if ([view isKindOfClass:[UILabel class]]) {
-        self.text = view.text;
-        self.font = view.font;
-        self.textColor = view.textColor;
-        self.shadowColor = view.shadowColor;
-        self.shadowOffset = view.shadowOffset;
-        self.textAlignment = view.textAlignment;
-        self.lineBreakMode = view.lineBreakMode;
-        self.attributedText = view.attributedText;
-        self.highlightedTextColor = view.highlightedTextColor;
-        self.numberOfLines = view.numberOfLines;
-        self.adjustsFontSizeToFitWidth = view.adjustsFontSizeToFitWidth;
-        self.baselineAdjustment = view.baselineAdjustment;
-        self.minimumScaleFactor = view.minimumScaleFactor;
-        self.preferredMaxLayoutWidth = view.preferredMaxLayoutWidth;
-        self.highlighted = view.highlighted;
-        self.enabled = view.enabled;
-        self.tintColor = view.tintColor;
+public extension Morphable where Self: UILabel {
+    
+    func copyProperties(from view: UIView) {
+        (self as UIView).copyConstraints(from: view)
+        if let label = view as? UILabel {
+            text = label.text
+            font = label.font
+            textColor = label.textColor
+            shadowColor = label.shadowColor
+            shadowOffset = label.shadowOffset
+            textAlignment = label.textAlignment
+            lineBreakMode = label.lineBreakMode
+            attributedText = label.attributedText
+            highlightedTextColor = label.highlightedTextColor
+            numberOfLines = label.numberOfLines
+            adjustsFontSizeToFitWidth = label.adjustsFontSizeToFitWidth
+            baselineAdjustment = label.baselineAdjustment
+            minimumScaleFactor = label.minimumScaleFactor
+            preferredMaxLayoutWidth = label.preferredMaxLayoutWidth
+            isHighlighted = label.isHighlighted
+            isEnabled = label.isEnabled
+            tintColor = label.tintColor;
+        }
     }
+    
 }
 
-@end
-
-@implementation UIImageView (BFWMorph)
-
-- (void)copyPropertiesFromView:(UIImageView*)view {
-    [super copyPropertiesFromView:view];
-    if ([view isKindOfClass:[UIImageView class]]) {
-        self.image = view.image;
-        self.highlightedImage = view.highlightedImage;
-        self.highlighted = view.highlighted;
-        self.animationImages = view.animationImages;
+public extension Morphable where Self: UIImageView {
+    
+    func copyProperties(from view: UIView) {
+        (self as UIView).copyConstraints(from: view)
+        if let imageView = view as? UIImageView {
+            image = imageView.image
+            highlightedImage = imageView.highlightedImage
+            isHighlighted = imageView.isHighlighted
+            animationImages = imageView.animationImages
+        }
     }
+    
 }
-
-@end
