fda6740c3dfc7c8b0f740699e0111b23b7bceaa3
Tom Brodhurst-Hill <developer@barefeetware.com>
2017-07-07 10:14:06 +1000

    UIViewController removeDismiss()

--- BFWControls/Modules/Transition/View/UIViewController+Dismiss.swift
+++ BFWControls/Modules/Transition/View/UIViewController+Dismiss.swift
@@ -4,14 +4,24 @@
 //
 //  Created by Tom Brodhurst-Hill on 12/5/17.
 //  Copyright © 2017 BareFeetWare. All rights reserved.
+//  Free to use at your own risk, with acknowledgement to BareFeetWare.
 //
 
 import UIKit
 
-extension UIViewController {
+public extension UIViewController {
     
-    @IBAction func dismiss(_ sender: UIControl?) {
+    @IBAction public func dismiss(_ sender: UIControl?) {
         dismiss(animated: true) {}
+    }
+    
+    public func removeDismiss() {
+        // TODO: Cater for multiple leftBarButtonItems.
+        if let selector = navigationItem.leftBarButtonItem?.action,
+            selector == #selector(UIViewController.dismiss(_:))
+        {
+            navigationItem.leftBarButtonItem = nil
+        }
     }
     
 }
