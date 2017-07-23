66c82397b2f7756bf288ba1f608367822d8c0ed0
Tom Brodhurst-Hill <developer@barefeetware.com>
2017-07-24 09:48:59 +1000

    ShadableView: sets background color (untested)

--- BFWControls/Modules/Shadable/View/ShadableView.swift
+++ BFWControls/Modules/Shadable/View/ShadableView.swift
@@ -9,10 +9,30 @@
 
 import UIKit
 
-/// UIView for grouping subviews under a single Shadable setting. eg view for a UINavigationController subclass.
+/// UIView for grouping subviews under a single Shadable setting.
 open class ShadableView: UIView, Shadable {
 
-    @IBInspectable open var isLight: Bool = false
-    @IBInspectable open var isLightAuto: Bool = true
+    @IBInspectable open var isLight: Bool = false { didSet { setNeedsUpdateView() }}
+    @IBInspectable open var isLightAuto: Bool = true { didSet { setNeedsUpdateView() }}
+    
+    private var needsUpdateView = false
+    
+    private func setNeedsUpdateView() {
+        needsUpdateView = true
+        setNeedsLayout()
+    }
+    
+    func updateViewIfNeeded() {
+        if needsUpdateView {
+            needsUpdateView = false
+            applyShade()
+        }
+    }
+    
+    public var lightColors: [UIColor] = [.white, .darkGray, .black]
+    
+    private func applyShade() {
+        backgroundColor = shadeColor
+    }
 
 }
