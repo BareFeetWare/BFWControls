1c40d71c82e4a25fd1418bd7c2f84011fbe0282e
Tom Brodhurst-Hill <developer@barefeetware.com>
2017-12-16 14:48:54 +1100

    UISegmentedControl: selectedTitle

---
+++ BFWControls/Modules/Controls/View/UISegmentedControl+Convenience.swift
@@ -0,0 +1,23 @@
+//
+//  UISegmentedControl+Convenience.swift
+//  BFWControls
+//
+//  Created by Tom Brodhurst-Hill on 16/12/17.
+//  Copyright © 2017 BareFeetWare. All rights reserved.
+//  Free to use at your own risk, with acknowledgement to BareFeetWare.
+//
+
+import UIKit
+
+public extension UISegmentedControl {
+    public var selectedTitle: String {
+        get {
+            return titleForSegment(at: selectedSegmentIndex)!
+        }
+        set {
+            guard let index = (0 ..< numberOfSegments).first(where: { titleForSegment(at: $0) == newValue } )
+                else { return }
+            selectedSegmentIndex = index
+        }
+    }
+}
