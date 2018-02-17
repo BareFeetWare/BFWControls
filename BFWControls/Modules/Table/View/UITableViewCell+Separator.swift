401eeeeeca882c8a02179b95cb5b78a5c069074b
Tom Brodhurst-Hill <developer@barefeetware.com>
2018-02-17 15:16:39 +1100

    Simplified using hiddenInsetRight instead of hiddenInset

--- BFWControls/Modules/Table/View/UITableViewCell+Separator.swift
+++ BFWControls/Modules/Table/View/UITableViewCell+Separator.swift
@@ -13,25 +13,21 @@ import UIKit
 public extension UITableViewCell {
     
     /// Arbitrarily large number. Must be larger than any possible screen dimension. Can't be .greatestFiniteMagnitude, since that causes a screen flash when the app is restored from background.
-    private static let offScreen: CGFloat = 999999.0
-
-    private static let hiddenInset = UIEdgeInsets(top: 0.0, left: 0.0, bottom: 0.0, right: offScreen)
-    
-    private var hiddenInset: UIEdgeInsets {
-        return type(of: self).hiddenInset
+    private var hiddenInsetRight: CGFloat {
+        return 999999.0
     }
-    
+
     /// Hides the separator in a cell in a table view that is showing cell separators.
     @IBInspectable public var isSeparatorHidden: Bool {
         get {
-            return separatorInset == hiddenInset
+            return separatorInset.right == hiddenInsetRight
         }
         set {
-            guard let inset: UIEdgeInsets = newValue
-                ? hiddenInset
-                : superviewTableView?.separatorInset
+            guard let insetRight: CGFloat = newValue
+                ? hiddenInsetRight
+                : superviewTableView?.separatorInset.right
                 else { return }
-            separatorInset = inset
+            separatorInset.right = insetRight
         }
     }
     
