1682820d3a36d14115df115789d01f1d8f97a786
Tom Brodhurst-Hill <developer@barefeetware.com>
2018-05-02 07:43:45 +1000

    DirectNibTableViewCell subclasses CustomTableViewCell

--- BFWControls/Modules/NibView/View/DirectNibTableViewCell.swift
+++ BFWControls/Modules/NibView/View/DirectNibTableViewCell.swift
@@ -4,20 +4,14 @@
 //
 //  Created by Tom Brodhurst-Hill on 24/4/18.
 //  Copyright © 2018 BareFeetWare. All rights reserved.
-//  Free to use and modify, without warranty.
+//  Free to use at your own risk, with acknowledgement to BareFeetWare.
 //
 
 import UIKit
 
-@IBDesignable open class DirectNibTableViewCell: UITableViewCell {
+@IBDesignable open class DirectNibTableViewCell: CustomTableViewCell {
 
-    // MARK: - Overriding storage
-    
-    private var overridingTextLabel: UILabel?
-    private var overridingDetailTextLabel: UILabel?
-    private var overridingImageView: UIImageView?
-    
-    // MARK: - Init and awake
+    // MARK: - Show nib content inside IB instance
     
     open override func prepareForInterfaceBuilder() {
         super.prepareForInterfaceBuilder()
@@ -36,35 +30,6 @@ import UIKit
             cell.copySubviewProperties(from: self)
         }
         return view
-    }
-    
-    // MARK: - UITableViewCell
-    
-    @IBOutlet open override var textLabel: UILabel? {
-        get {
-            return overridingTextLabel ?? super.textLabel
-        }
-        set {
-            overridingTextLabel = newValue
-        }
-    }
-
-    @IBOutlet open override var detailTextLabel: UILabel? {
-        get {
-            return overridingDetailTextLabel ?? super.detailTextLabel
-        }
-        set {
-            overridingDetailTextLabel = newValue
-        }
-    }
-
-    @IBOutlet open override var imageView: UIImageView? {
-        get {
-            return overridingImageView ?? super.imageView
-        }
-        set {
-            overridingImageView = newValue
-        }
     }
     
 }
