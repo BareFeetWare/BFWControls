71271f79e5e222e52c8533b5056b0cdd5b74f94f
Tom Brodhurst-Hill <developer@barefeetware.com>
2018-05-01 14:39:18 +1000

    keepingTraitsAndColorButAdding: moved from StyledText.swift to NSAttributedString+TextAttributes.swift

--- BFWControls/Modules/Styled/View/StyledText.swift
+++ BFWControls/Modules/Styled/View/StyledText.swift
@@ -320,28 +320,3 @@ public extension UIFont {
         return font
     }
 }
-
-public extension NSAttributedString {
-    public func keepingTraitsAndColorButAdding(attributes: TextAttributes) -> NSAttributedString {
-        let attributedString = NSMutableAttributedString(string: string, attributes: attributes)
-        enumerateAttributes(in: NSRange(location: 0, length: length), options: [])
-        { (attributes, range, stop) in
-            if let color = attributes[NSAttributedStringKey.foregroundColor] as? UIColor,
-                ![UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1), .black].contains(color)
-            {
-                attributedString.addAttributes([NSAttributedStringKey.foregroundColor : color], range: range)
-            }
-            if let font = attributes[NSAttributedStringKey.font] as? UIFont,
-                !font.fontDescriptor.symbolicTraits.isEmpty,
-                let oldFont = attributedString.attribute(NSAttributedStringKey.font,
-                                                         at: range.location,
-                                                         longestEffectiveRange: nil,
-                                                         in: range) as? UIFont
-            {
-                let newFont = oldFont.addingSymbolicTraits(font.fontDescriptor.symbolicTraits)
-                attributedString.addAttributes([NSAttributedStringKey.font : newFont], range: range)
-            }
-        }
-        return NSAttributedString(attributedString: attributedString)
-    }
-}
