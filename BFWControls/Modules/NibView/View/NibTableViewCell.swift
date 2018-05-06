49c4c2d86cc9e003cd35cd0c829173bf54555544
Tom Brodhurst-Hill <developer@barefeetware.com>
2018-05-06 11:07:26 +1000

    NibTableViewCell: NibReplaceableand replacedByNibView()

--- BFWControls/Modules/NibView/View/NibTableViewCell.swift
+++ BFWControls/Modules/NibView/View/NibTableViewCell.swift
@@ -10,12 +10,64 @@
 import UIKit
 
 @IBDesignable open class NibTableViewCell: CustomTableViewCell {
-
+    
+    // MARK: - IBOutlets
+    
+    // TODO: Perhaps integrate actionView with accessoryView
+    
+    @IBOutlet open var actionView: UIView?
+    
+    // MARK: - Variables
+    
+    @IBInspectable open var isActionViewHidden: Bool {
+        get {
+            return actionView?.isHidden ?? true
+        }
+        set {
+            actionView?.isHidden = newValue
+        }
+    }
+    
+    /// Override to give different nib for each cell style
+    @IBInspectable open var nibName: String?
+    
+    open var style: UITableViewCellStyle = .default
+    
+    open var contentSubview: UIView? {
+        guard let contentSubview = contentView.subviews.first,
+            contentView.subviews.count == 1
+            else { return nil }
+        return contentSubview
+    }
+    
+    // MARK: - Functions
+    
+    // TODO: Move to NibReplaceable:
+    
+    open func replacedByNibView() -> UIView {
+        return replacedByNibView(fromNibNamed: nibName ?? type(of: self).nibName)
+    }
+    
+    // MARK: - Init
+    
+    public override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.style = style
+    }
+    
+    public required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        let styleInt = coder.decodeInteger(forKey: "UITableViewCellStyle")
+        if let style = UITableViewCellStyle(rawValue: styleInt) {
+            self.style = style
+        }
+    }
+    
     // MARK: - Show nib content inside IB instance
     
     open override func prepareForInterfaceBuilder() {
         super.prepareForInterfaceBuilder()
-        guard let cell = viewFromNib as? UITableViewCell
+        guard let cell = replacedByNibView() as? UITableViewCell
             else { return }
         cell.copySubviewProperties(from: self)
         contentView.isHidden = true
@@ -25,11 +77,20 @@ import UIKit
     }
     
     open override func awakeAfter(using coder: NSCoder) -> Any? {
-        let view = viewFromNib
+        let view = replacedByNibView()
         if let cell = view as? UITableViewCell {
             cell.copySubviewProperties(from: self)
         }
+        (view as? NibReplaceable)?.removePlaceholders()
         return view
+    }
+    
+}
+
+extension NibTableViewCell: NibReplaceable {
+    
+    open var placeholderViews: [UIView] {
+        return [textLabel, detailTextLabel].compactMap { $0 }
     }
     
 }
