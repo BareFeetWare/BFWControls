4d67e2b038696c1527dd06a09f857efb61b23532
Tom Brodhurst-Hill <developer@barefeetware.com>
2018-06-18 08:23:52 +1000

    UIViewController+Unwind: unwindToSelf(), unwindToRoot(), frontViewController

---
+++ BFWControls/Modules/Transition/Controller/UIViewController+Unwind.swift
@@ -0,0 +1,49 @@
+//
+//  UIViewController+Unwind.swift
+//  BFWControls
+//
+//  Created by Tom Brodhurst-Hill on 17/6/18.
+//  Copyright © 2018 BareFeetWare. All rights reserved.
+//  Free to use at your own risk, with acknowledgement to BareFeetWare.
+//
+
+public extension UIViewController {
+    
+    public var frontViewController: UIViewController {
+        let frontViewController = presentedViewController
+            ?? navigationController?.topViewController
+            ?? (self as? UINavigationController)?.topViewController
+            ?? self
+        return frontViewController == self
+            ? self
+            : frontViewController.frontViewController
+    }
+    
+    public func unwindToSelf(animated: Bool, completion: (() -> Void)? = nil) {
+        if let _ = presentedViewController {
+            dismiss(animated: animated) { [weak self] in
+                guard let strongSelf = self
+                    else { return }
+                strongSelf.popToRoot(animated: animated)
+                completion?()
+            }
+        } else {
+            popToRoot(animated: animated)
+            completion?()
+        }
+    }
+    
+    private func popToRoot(animated: Bool) {
+        if let navigationController = self as? UINavigationController
+            ?? navigationController
+        {
+            navigationController.popToRootViewController(animated: animated)
+        }
+        
+    }
+    
+    @objc public func unwindToRoot(animated: Bool) {
+        view.window!.rootViewController!.unwindToSelf(animated: animated)
+    }
+    
+}
