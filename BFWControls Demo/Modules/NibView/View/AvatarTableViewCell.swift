bef3a4d49e478d7afd9ce804f820d8a370671113
Tom Brodhurst-Hill <developer@barefeetware.com>
2018-08-05 18:01:26 +1000

    AvatarTableViewCell: removed tertiaryTextLabel and tertiaryText since they are now provided by the superclass

--- BFWControls Demo/Modules/NibView/View/AvatarTableViewCell.swift
+++ BFWControls Demo/Modules/NibView/View/AvatarTableViewCell.swift
@@ -4,22 +4,12 @@
 //
 //  Created by Tom Brodhurst-Hill on 26/4/18.
 //  Copyright © 2018 BareFeetWare. All rights reserved.
+//  Free to use at your own risk, with acknowledgement to BareFeetWare.
 //
 
 import UIKit
 import BFWControls
 
 @IBDesignable class AvatarTableViewCell: NibTableViewCell {
-
-    @IBOutlet var tertiaryTextLabel: UILabel?
-    
-    @IBInspectable var tertiaryText: String? {
-        get {
-            return tertiaryTextLabel?.text
-        }
-        set {
-            tertiaryTextLabel?.text = newValue
-        }
-    }
 
 }
