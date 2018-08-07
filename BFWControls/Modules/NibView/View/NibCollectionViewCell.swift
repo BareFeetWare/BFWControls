e0a5d5a85210803986b3ed5c5ff66bb6d4bfd1cb
Tom Brodhurst-Hill <developer@barefeetware.com>
2018-08-07 13:18:51 +1000

    NibCollectionViewCell: Added awakeAfter(using coder:), so it loads nib content at runtime.

--- BFWControls/Modules/NibView/View/NibCollectionViewCell.swift
+++ BFWControls/Modules/NibView/View/NibCollectionViewCell.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Tom Brodhurst-Hill on 14/8/17.
 //  Copyright © 2017 BareFeetWare. Free to use and modify, without warranty.
+//  Free to use at your own risk, with acknowledgement to BareFeetWare.
 //
 
 import UIKit
@@ -15,4 +16,15 @@ open class NibCollectionViewCell: BFWNibCollectionViewCell {
     @objc open func replacedByNibView() -> UIView {
         return replacedByNibView(fromNibNamed: type(of: self).nibName)
     }
+    
+    // MARK: - Init
+    
+    open override func awakeAfter(using coder: NSCoder) -> Any? {
+        let view = replacedByNibView()
+        if view != self {
+            (view as? NibReplaceable)?.removePlaceholders()
+        }
+        return view
+    }
+    
 }
