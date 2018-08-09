eb1e774235acc2b8dc33ac9e1748549133c78b11
Tom Brodhurst-Hill <developer@barefeetware.com>
2018-08-09 18:23:50 -0400

    ArcTableViewCell

---
+++ BFWControls Demo/Modules/Presentation/View/ArcTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ArcTableViewCell.swift
+//  BFWControls Demo
+//
+//  Created by Tom Brodhurst-Hill on 9/8/18.
+//  Copyright © 2018 BareFeetWare. All rights reserved.
+//
+
+import UIKit
+
+class ArcTableViewCell: UITableViewCell {
+
+    
+}
