7aca4bbc00b89535aeef159fadf3a33f2acaf3d6
Tom Brodhurst-Hill <developer@barefeetware.com>
2018-09-29 16:53:57 +1000

    UIView+Overlay: tidied generics

--- BFWControls/Modules/Overlay/View/UIView+Overlay.swift
+++ BFWControls/Modules/Overlay/View/UIView+Overlay.swift
@@ -11,44 +11,28 @@ import UIKit
 
 public extension UIView {
     
-    public func subview<T: UIView>(ofType: T.Type) -> T? {
+    public func firstSubview<T: UIView>(ofType: T.Type) -> T? {
         return subviews.first { $0 is T } as? T
     }
     
-    public func addOverlay<T: UIView>(ofType: T.Type, backgroundColor: UIColor? = nil) {
-        if subview(ofType: T.self) == nil {
-            let subview = T()
-            if let backgroundColor = backgroundColor {
-                subview.backgroundColor = backgroundColor
-            }
-            addSubview(subview)
-            subview.pinToSuperviewEdges()
-        }
-    }
-
-    public func removeOverlay<T: UIView>(ofType: T.Type) {
-        subview(ofType: T.self)?.removeFromSuperview()
-    }
-    
-    public func addOverlay<T: UIView>(
-        ofType: T.Type,
-        timeInterval: TimeInterval,
-        backgroundColor: UIColor? = nil)
-    {
-        addOverlay(ofType: T.self, backgroundColor: backgroundColor)
+    public func addOneOverlay<T: UIView>(_ overlay: T, timeInterval: TimeInterval) {
+        guard firstSubview(ofType: T.self) == nil
+            else { return }
+        addSubview(overlay)
+        overlay.pinToSuperviewEdges()
         let timer: Timer
         if #available(iOS 10.0, *) {
             timer = Timer(
                 fire: Date().addingTimeInterval(timeInterval),
-                interval: 1.0,
+                interval: 0.0,
                 repeats: false)
-            { [weak self] _ in
-                self?.removeOverlay(ofType: T.self)
+            { _ in
+                overlay.removeFromSuperview()
             }
         } else {
             timer = Timer(
                 timeInterval: timeInterval,
-                target: subview(ofType: T.self)!,
+                target: overlay,
                 selector: #selector(removeFromSuperview),
                 userInfo: nil,
                 repeats: false)
