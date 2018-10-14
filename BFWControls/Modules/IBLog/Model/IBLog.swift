a2a5ab2c37b3d05a288ba8c26c58e0827a6b49f9
Tom Brodhurst-Hill <developer@barefeetware.com>
2018-10-14 14:10:29 +1100

    IBLog: dateFormatter; removed application name; indented

--- BFWControls/Modules/IBLog/Model/IBLog.swift
+++ BFWControls/Modules/IBLog/Model/IBLog.swift
@@ -11,22 +11,34 @@ import Foundation
 
 open class IBLog {
     
-    /// Write message to a log file.Useful for IBDesignable debugging, since the normal print() doesn't log to the Xcode console.
+    private static var indentPosition = 0
+    private static let dateFormatter: DateFormatter = {
+        var dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd hh:mm:ss.SSS"
+        return dateFormatter
+    }()
+    
+    /// Write message to a log file. Useful for IBDesignable debugging, since the normal print() doesn't log to the Xcode console.
     public static func write(_ message: String,
-                             filePath: String = "/tmp/IBDesignable.log")
+                             indent: Int = 0,
+                             toFilePath filePath: String = "/tmp/IBDesignable.log")
     {
         #if TARGET_INTERFACE_BUILDER
         if !FileManager.default.fileExists(atPath: filePath) {
             FileManager.default.createFile(atPath: filePath, contents: Data(), attributes: nil)
         }
-        
         let fileHandle = FileHandle(forWritingAtPath: filePath)!
         fileHandle.seekToEndOfFile()
-        
+        if indent < 0 {
+            indentPosition += indent
+        }
         let date = Date()
-        let bundle = Bundle(for: self)
-        let applicationName: AnyObject = bundle.object(forInfoDictionaryKey: "CFBundleName") as AnyObject
-        let data = "\(date) \(applicationName) \(message)\n"
+        let dateString = dateFormatter.string(from: date)
+        let indentString = String(repeating: "  ", count: indentPosition)
+        if indent > 0 {
+            indentPosition += indent
+        }
+        let data = "\(dateString) \(indentString) \(message)\n"
             .data(using: String.Encoding.utf8, allowLossyConversion: true)!
         fileHandle.write(data)
         #endif
