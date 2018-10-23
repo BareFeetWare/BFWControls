952a5e3c2def16980434884987cecadf8e759d4d
Tom Brodhurst-Hill <developer@barefeetware.com>
2018-10-23 12:08:47 +1100

    keepingTraitsAndColorButAdding(): calls IBLog.write()

--- BFWControls/Modules/Styled/Model/NSAttributedString+TextAttributes.swift
+++ BFWControls/Modules/Styled/Model/NSAttributedString+TextAttributes.swift
@@ -11,12 +11,14 @@ import Foundation
 
 public extension NSAttributedString {
     public func keepingTraitsAndColorButAdding(attributes: TextAttributes) -> NSAttributedString {
+        IBLog.write("keepingTraitsAndColorButAdding()", indent: 1)
         let attributedString = NSMutableAttributedString(string: string, attributes: attributes)
         enumerateAttributes(in: NSRange(location: 0, length: length), options: [])
         { (attributes, range, stop) in
             if let color = attributes[NSAttributedString.Key.foregroundColor] as? UIColor,
                 ![UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1), .black].contains(color)
             {
+                IBLog.write("addAttributes([Key.foregroundColor : \(color), range: \(range))")
                 attributedString.addAttributes([NSAttributedString.Key.foregroundColor : color], range: range)
             }
             if let font = attributes[NSAttributedString.Key.font] as? UIFont,
@@ -27,9 +29,11 @@ public extension NSAttributedString {
                                                          in: range) as? UIFont
             {
                 let newFont = oldFont.addingSymbolicTraits(font.fontDescriptor.symbolicTraits)
+                IBLog.write("addAttributes([Key.font : \(newFont), range: \(range))")
                 attributedString.addAttributes([NSAttributedString.Key.font : newFont], range: range)
             }
         }
+        IBLog.write("keepingTraitsAndColorButAdding() return", indent: -1)
         return NSAttributedString(attributedString: attributedString)
     }
 }
